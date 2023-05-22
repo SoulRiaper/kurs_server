447da59661b0dc9aca649ccac101d0e537f4cb8c
SoulRiaper <soulriaper@gmail.com>
2023-05-22 16:53:03 +0300

    add last value + dialog window

---
+++ DialogUi.py
@@ -0,0 +1,7 @@
+from UI_ComDialog import Ui_Dialog
+import random, time
+
+class DialogUi(Ui_Dialog):
+    
+      def retranslateUi(self, Dialog):
+            super().retranslateUi(Dialog)
--- MainUi.py
+++ MainUi.py
@@ -1,12 +1,32 @@
 from UI_MainWindow import Ui_MainWindow
 from serial.tools.list_ports import comports
+from DialogUi import DialogUi
+from PyQt6.QtWidgets import QDialog
+import random
 
 class MainUi(Ui_MainWindow):
-    
-    def retranslateUi(self, MainWindow):
-        super().retranslateUi(MainWindow)
-        ports = comports()
-        if len(ports) == 0:
-            return
-        for port in ports:
-            self.comPorts.addItems(port.name)
+
+      def setupUi(self, MainWindow):
+            super().setupUi(MainWindow)
+            self.portListenButton.clicked.connect(self.show_comPort_window)
+            self.ListenPortButton.clicked.connect(self.updateValue)
+
+            dialog = DialogUi()
+            dialog_ui = QDialog()
+            dialog.setupUi(dialog_ui)
+            self.dialog = dialog_ui
+
+      def updateValue(self, value: int):
+            value = random.randint(0, 10)
+            self.LastValue.setText(str(value))
+
+      def show_comPort_window(self):
+            self.dialog.show()
+
+      def retranslateUi(self, MainWindow):
+            super().retranslateUi(MainWindow)
+            ports = comports()
+            if len(ports) == 0:
+                  return
+            for port in ports:
+                  self.comPorts.addItems(port.name)
